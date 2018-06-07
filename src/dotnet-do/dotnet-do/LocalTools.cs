c092f4b84ffbf0d481b0ce8880a0ade1e392805e
Rich Lander <rlander@microsoft.com>
2018-06-07 11:13:33 -0700

    Update implementation

--- src/dotnet-do/dotnet-do/LocalTools.cs
+++ src/dotnet-do/dotnet-do/LocalTools.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace dotnet_do
 {
@@ -10,26 +12,75 @@ namespace dotnet_do
         private DirectoryInfo _dir;
         private Dictionary<string,Tool> tools;
         private static readonly string TOOLS_LIST_FILENAME = "dotnet.tools";
+        private static readonly string TOOLS_DIRECTORY_NAME = ".dotnettools";
+
         public LocalTools(DirectoryInfo location)
         {
             _dir = location;
         }
 
-        public IEnumerable<Tool> GetTools()
+        public string ToolPath
         {
+            get; private set;
+        }
+
+        public void Run(string[] args)
+        {
+            var dir = _dir;
+            var exePath = string.Empty;
+            var exeName = args[0];
+            do
+            {
+                var toolPath = Path.Combine(dir.FullName, TOOLS_DIRECTORY_NAME);
+                var toolsInstallExist = Directory.Exists(toolPath);
+                if (toolsInstallExist)
+                {
+                    exePath = Path.Combine(toolPath, exeName);
+                    break;
+                }
+
+                dir = dir.Parent;
+
+            } while (dir != null);
+
+            string argsString = string.Empty;
+            if (args.Length >1)
+            {
+                var argSpan = new Span<string>(args, 1, args.Length);
+                argsString = String.Join(' ', argSpan.ToArray());
+            }
+
+            if (exePath != string.Empty)
+            {
+                Process.Start(exePath,argsString);
+            }
+            else
+            {
+                Console.WriteLine("Tool not found.");
+            }
+
+        }
+
+        public void Install()
+        {
+            var dir = _dir;
+
             tools = new Dictionary<string, Tool>();
 
-            DirectoryInfo dir = _dir;
-            while (dir.Parent != null)
+            do
             {
-                dir = dir.Parent;
-                var toolsListExist = File.Exists(TOOLS_LIST_FILENAME);
+                var toolPath = Path.Combine(dir.FullName, TOOLS_LIST_FILENAME);
+                var toolsListExist = File.Exists(toolPath);
                 if (toolsListExist)
                 {
-                    var toolPath = Path.Combine(dir.FullName, TOOLS_LIST_FILENAME);
+                    if (ToolPath == null)
+                    {
+                        ToolPath = Directory.GetParent(toolPath).FullName;
+                    }
+
                     var dirTools = GetToolsFomPath(toolPath);
 
-                    foreach(var tool in dirTools)
+                    foreach (var tool in dirTools)
                     {
                         if (!tools.ContainsKey(tool.Name))
                         {
@@ -37,15 +88,28 @@ namespace dotnet_do
                         }
                     }
                 }
+
+                dir = dir.Parent;
+
+            } while (dir != null);
+
+            if (tools.Keys.Count == 0)
+            {
+                Console.WriteLine($"{TOOLS_DIRECTORY_NAME} was not found.");
+                return;
             }
 
-            foreach (var tool in tools)
+            foreach (var t in tools)
             {
-                yield return tool.Value;
+                var tool = t.Value;
+                var toolString = string.IsNullOrWhiteSpace(tool.Version) ? $"{tool.Name}" : $"{tool.Name} --version {tool.Version}";
+                var toolPath = Path.Combine(ToolPath, TOOLS_DIRECTORY_NAME);
+                var toolInstallParams = $"tool install --tool-path {toolPath} {toolString}";
+                Process.Start("dotnet", toolInstallParams);
             }
         }
 
-        public List<Tool> GetToolsFomPath(string toolsPath)
+        private List<Tool> GetToolsFomPath(string toolsPath)
         {
             var tools = new List<Tool>();
 
@@ -54,13 +118,19 @@ namespace dotnet_do
                 string line = "";
                 while ((line = reader.ReadLine()) != null)
                 {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+                    else if (line[0] == '#')
+                    {
+                        continue;
+                    }
+
                     var tool = new Tool();
                     var array = line.Split(',');
-                    if (array.Length>2)
-                    {
-                        throw new Exception();
-                    }
-                    else if (array.Length == 2)
+
+                    if (array.Length > 1)
                     {
                         tool.Name = array[0];
                         tool.Version = array[1];
--- src/dotnet-do/dotnet-do/Program.cs
+++ src/dotnet-do/dotnet-do/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace dotnet_do
 {
@@ -7,7 +9,23 @@ namespace dotnet_do
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var currentDir = Directory.GetCurrentDirectory();
+            var dir = new DirectoryInfo(currentDir);
+            var tools = new LocalTools(dir);
 
+            if (args == null || args.Length == 0)
+            {
+                tools.Install();
+            }
+            else
+            {
+                tools.Run(args);
+            }
+        }
+
+        private static void Run()
+        {
+            
         }
 
 
